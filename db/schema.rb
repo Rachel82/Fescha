62514298c910c0f9e3ad4ae006e90048fff2a5c1
Rachel82 <akilimali82@hotmail.fr>
2017-12-03 13:12:05 +0100

    5.4 rake:db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20171126152929) do
 
   create_table "comments", force: :cascade do |t|
@@ -23,8 +22,6 @@ ActiveRecord::Schema.define(version: 20171126152929) do
     t.index ["product_id"], name: "index_comments_on_product_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
-
-
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
