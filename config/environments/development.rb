9ad06126c93b1c1c8461c8a0c6700d0af1757d22
Rachel82 <akilimali82@hotmail.fr>
2017-12-03 11:05:48 +0100

    Pull  (#4)
    * 5.6 Add Comment Model Association
    
    * 5.7 Add comment model and Scope
    
    * 5.6 Change Rating Asc code
    
    * 5.4 Add Styles Mailer
    
    * 5.4 Fix login button
    
    * 5.4 Change Products Image_tag
    
    * 5.4 Add Product Instance in Show Method
    
    * 5.4 Style Login
    
    * 5.4 Add config.logger
    
    * 5.4 Styles
    
    * 5.4 Add Sendgrid Environment.rb
    
    * Change User_mailer code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,4 +55,14 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
 
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.sendgrid.net',
+  port: '587',
+  authentication: :plain,
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: 'heroku.com',
+  enable_starttls_auto: true
+}
+
 
