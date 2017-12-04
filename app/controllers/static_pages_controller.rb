4afe7cde7a9cd28dc1cea1be2d9c59da187f25a8
Rachel82 <akilimali82@hotmail.fr>
2017-12-04 13:21:42 +0100

    5.4 Remove Method Thank_You

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,12 +9,7 @@ class StaticPagesController < ApplicationController
   def contact
   end
 
-  def thank_you
-  	@name = params[:name]
-  	@email = params[:email]
-  	@message = params[:message]
-  	UserMailer.contact_form(@email, @name, @message).deliver_now
-  end
+ 
 
   def landing_page
   		@products = Product.limit(3)
