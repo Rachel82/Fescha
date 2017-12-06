77a2190b89e217756c1720fb87735dfaa46143a4
Rachel82 <akilimali82@hotmail.fr>
2017-12-06 19:40:51 +0100

    5.5 Authorization

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
+    user ||= User.new # guest user (not logged in) shorthand for checking if a variable exists, and if not assign it to something else
     can :manage, User, id: user.id
   end
 end
