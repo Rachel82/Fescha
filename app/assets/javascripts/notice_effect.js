c282eb27af44c9b7c8e2c0e7a4533fbb606b18ee
Rachel82 <akilimali82@hotmail.fr>
2017-12-06 20:49:33 +0100

    5.5 Change Notice Link

--- app/assets/javascripts/notice_effect.js
+++ app/assets/javascripts/notice_effect.js
@@ -1,4 +1,4 @@
 $(document).on('turbolinks:load', function(){
 	console.log("It works on each visit!");
-	$("#alert, #notice, .alert").fadeOut(5000).delay(6000);
+	$("#alert, #notice, .alert").fadeOut(6000).delay(9000);
 })
