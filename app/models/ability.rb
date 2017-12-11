6fa196cb55292efdb7ba1b6832939c038cf2038f
Rachel82 <akilimali82@hotmail.fr>
2017-12-11 13:12:35 +0100

    5.10 Admin Role and Users Comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,10 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in) shorthand for checking if a variable exists, and if not assign it to something else
-    can :manage, User, id: user.id
+    if user.admin?
+    	can :manage, :all
+  	else
+  		can :manage, User, id: user.id
+  	end
   end
 end
