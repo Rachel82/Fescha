41d58711bbea8775f5057e9e2b55d0ad82f891ad
Rachel82 <akilimali82@hotmail.fr>
2017-12-11 13:35:06 +0100

    5.10 Change Ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,9 +4,9 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in) shorthand for checking if a variable exists, and if not assign it to something else
     if user.admin?
-    	can :manage, :all, :read,
+    	can :manage, :all, :read
   	else
-  		can :read, :all,
+  		can :manage, User, id: user.id
   	end
   end
 end
