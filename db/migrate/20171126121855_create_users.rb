76fee81839b3826f9ebd2226f2a1cccc2b3644ca
Rachel82 <akilimali82@hotmail.fr>
2017-12-11 19:41:54 +0100

    5.10 Comment out User_create file

--- db/migrate/20171126121855_create_users.rb
+++ db/migrate/20171126121855_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
-    create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
+    	#create_table :users do |t|
+      #t.string :first_name
+      #t.string :last_name
 
-      t.timestamps
+      #t.timestamps
     end
   end
 end
