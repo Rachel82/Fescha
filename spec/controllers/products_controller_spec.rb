7a91598807d8617ae4cff12654375770af2672a0
Rachel82 <akilimali82@hotmail.fr>
2017-12-20 18:35:35 +0100

    6.3 Controller_specs

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,82 +1,140 @@
 require 'rails_helper'
-	describe ProductsController, type: :controller do #creates new
+	describe ProductsController, type: :controller do 
+
 		let (:product) {Product.create!(name: 'Lola', description: 'Hellogfdbgddhfdghdhdfgdf', price:'50 Eur')}
 		let (:user) {User.create!(first_name: 'Hava', last_name: 'Hello', email: 'lola@yahoo.fr', password: 'hiya23456')}
-		#@user = User.create!(first_name: 'Sheva', last_name: 'Bella', email: 'loya@yahoo.fr', password: 'hiya23456')
+		let (:admin) {User.create!(first_name: 'Hav', last_name: 'Hell', email: 'loa@yahoo.fr', password: 'ya23456')}
 
-		describe 'GET #new:' do
-			context 'when user is logged in' do
-				before do
-  				sign_in user
-  			end
-				it 'creates new product' do
-					get :new
-					expect(response).to be_successful
+#===================NEW===================================================================
+	describe 'GET #new:' do
+		context 'when user is logged in' do
+			before do
+				sign_in user
+			end
+			it 'creates new product' do
+				get :new
+        expect(assigns(:product)).to be_a_new(Product)
+				expect(response).to be_successful
+			end
+		end
+	end 
+#===================CREATE===================================================================
+
+	describe 'Post #create' do
+		context 'when user is logged in ' do
+			before do 
+				sign_in user
+			end
+				it 'creates new product ' do
+					post :create, params: {product: { name: 'Tshirt', description: '', image_url: '', price: ''}}
+					expect(Product.count).to eq 1
+					expect(flash[:notice]).to eq "Product was successfully created."
 				end
 			end
-		end 
-			describe 'GET #index' do
-				context 'when page is loaded' do
-    			it 'renders the index page' do 
-      			get :index
-      			expect(response).to be_ok
-      			expect(response).to render_template('index')
-      		#expect(assigned(:product).to expect(assigns(:product)).to be_a_new(Product)/ or flash code.
-    			end
-  			end
-  		end
-  		describe 'Get #show' do
-  			context 'loads individual product' do
-  				it 'renders show page' do
-  					get :show, params: {id: product}
-      			expect(response).to be_ok
-      			expect(response).to render_template('show')
-					end
+		context 'when user is logged out ' do
+			before do 
+				sign_out user
+			end
+				it 'cannot create new product ' do
+					post :create, params: {product: { name: 'Tshirt', description: '', image_url: '', price: ''}}
+					expect(flash[:alert]).to eq 'You need to sign in or sign up before continuing.'
+					expect(response).to redirect_to('/users/sign_in')
 				end
 			end
-			describe 'Get #edit' do
-				context 'when user is logged in ' do
-					before do 
-						sign_in user
-					end
-					it 'access to edit ' do
-						get :edit, params: {id: product}
-						expect(response).to be_ok
-						#expect(flash[:notice]).to eq 'Product was successfully updated.'
-						expect(response).to render_template('products/edit')
-					end
-				end
+		end
+#===================INDEX===================================================================
+
+	describe 'GET #index' do
+		context 'when page is loaded' do
+			it 'renders the index page' do 
+  			get :index
+  			expect(response).to be_ok
+  			expect(response).to render_template('index')
 			end
-				context 'when user is logged in ' do
-					before do 
-						sign_out user
-					end
-					it 'cannot access #edit ' do
-						get :edit, params: {id: product}
-						expect(response).not_to be_ok
-						expect(response).to redirect_to('/users/sign_in')
-					end
-				end
+		end
+	end
+#===================SHOW===================================================================
+
+	describe 'Get #show' do
+		context 'loads individual product' do
+			it 'renders show page' do
+				get :show, params: {id: product}
+  			expect(response).to be_ok
+  			expect(response).to render_template('show')
+			end
+		end
+	end
+
+#===================EDIT===================================================================
+
+	describe 'Get #edit' do
+		context 'when user is logged in ' do
+			before do 
+				sign_in user
+			end
+			it 'access to edit ' do
+				get :edit, params: {id: product}
+				expect(response).to be_ok
+				#expect(flash[:notice]).to eq 'Product was successfully updated.'
+				expect(response).to render_template('products/edit')
+			end
+		end
+	
+		context 'when user is not logged in ' do
+			before do 
+				sign_out user
+			end
+			it 'cannot access #edit ' do
+				get :edit, params: {id: product}
+				expect(response).not_to be_ok
+				expect(response).to redirect_to('/users/sign_in')
+			end
+		end
+	end
+#===================UPDATE===================================================================
+
+	describe 'Patch #update' do
+		context 'when user is logged in ' do
+				let (:user) {User.create!(first_name: 'Hava', last_name: 'Hello', email: 'lola@yahoo.fr', password: 'hiya23456')}
+				let (:product) {Product.create!(name: 'Lola', description: 'Hellogfdbgddhfdghdhdfgdf', price:'50 Eur')}
+		
+			before do 
+				sign_in user
 			end
 
-			describe 'Put #update' do
-				context 'when user is logged in ' do
-						let (:user) {User.create!(first_name: 'Hava', last_name: 'Hello', email: 'lola@yahoo.fr', password: 'hiya23456')}
-						let (:product) {Product.create!(name: 'Lola', description: 'Hellogfdbgddhfdghdhdfgdf', price:'50 Eur')}
-
-					before do 
-						sign_in user
-					end
-
-					it 'access to edit ' do
-						put :update, params: {id: product, product: {name: 'T'}}
-						expect(response).to be_ok
-						expect(product.name).to eq 'T'
-						#expect(flash[:notice]).to eq 'Product was successfully updated.'
-						expect(response).to render_template('products/edit')
-					end
-				end
+			it 'update product' do
+				patch :update, params: {id: product.id, product: {name: 'T'}}
+				expect(Product.where("id = ?", product.id).first.name).to eq "T"
+				expect(flash[:notice]).to eq 'Product was successfully updated.'
+				expect(response).to redirect_to product_path
 			end
-			
+		end
+		context 'when user is not logged in ' do
+			before do 
+				sign_out user
+			end
+			it 'cannot update product' do
+				patch :update, params: {id: product.id, product: {name: 'T'}}
+				expect(response).not_to be_ok
+				expect(flash[:alert]).to eq 'You need to sign in or sign up before continuing.'
+				expect(response).to redirect_to('/users/sign_in')
+			end
+		end
+	end
 
+#===================DELETE===================================================================
 
+	describe 'Delete #destroy' do
+		context 'when user admin is logged in ' do
+			before do 
+				sign_in admin
+			end
+			it 'destroys product'	do
+				delete :destroy, params: {id: product.id}
+				expect(flash[:notice]).to eq "Product was successfully destroyed."
+				expect(Product.where("id = ?", product.id)).not_to exist
+				expect(Product.count).to eq 0
+			end
+		end
+	end
+end
