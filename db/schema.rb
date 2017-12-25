5b64a00785cd18000766263870a8625d5daa1f03
Rachel82 <akilimali82@hotmail.fr>
2017-12-25 20:39:46 +0100

    6.5 Payment Cents

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171211110306) do
+ActiveRecord::Schema.define(version: 20171225190647) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 20171211110306) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "colour"
-    t.integer "price"
+    t.integer "price_in_cents"
   end
 
   create_table "users", force: :cascade do |t|
