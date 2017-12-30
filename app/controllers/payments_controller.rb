d349bf9b8c357664c58ac6fc3acf9cbd784a7c0f
Rachel82 <akilimali82@hotmail.fr>
2017-12-30 11:20:33 +0100

    6.6 Change Controller Payment

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -8,14 +8,14 @@ class PaymentsController < ApplicationController
   # Create the charge on Stripe's servers - this will charge the user's card
   begin
     charge = Stripe::Charge.create(
-      amount: (@product.price_in_cents/100), # amount in cents, again
+      amount: (@product.price_in_cents), # amount in cents, again
       currency: 'eur',
       source: token,
       description: params[:stripeEmail]
     )
     if charge.paid
     	Order.create(product_id: @product.id, user_id: @user.id,
-      total: @product.price)
+      total: @product.price_in_cents)
   	end
 
   rescue Stripe::CardError => e
@@ -24,7 +24,7 @@ class PaymentsController < ApplicationController
     err = body[:error]
     flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
   end
-  redirect_to product_path(product)
+  redirect_to product_path(@product)
 end
 
 def index
