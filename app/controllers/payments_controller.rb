14dd953b78da28c601c4ec0d35f7d847fd04caf1
Rachel82 <akilimali82@hotmail.fr>
2018-01-06 12:59:39 +0100

    6.7 ByeBug

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,6 +6,7 @@ class PaymentsController < ApplicationController
 
   	token = params[:stripeToken]
   # Create the charge on Stripe's servers - this will charge the user's card
+
   begin
     charge = Stripe::Charge.create(
       amount: (@product.price_in_cents), # amount in cents, again
@@ -24,6 +25,7 @@ class PaymentsController < ApplicationController
     err = body[:error]
     flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
   end
+  byebug
   redirect_to product_path(@product)
 end
 
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,8 @@ class ProductsController < ApplicationController
       @products = Product.search(search_term)
     else
       @products = Product.all
+      byebug
+      #logger.debug "Product: #{@products}"
     end
   end
 
@@ -17,6 +19,7 @@ class ProductsController < ApplicationController
   # GET /products/1.json
   def show
     @comments = @product.comments.order("created_at DESC").paginate(:page => params[:page], :per_page =>5)
+    # logger.debug "Comments: #{@comments}"
   end
 
   # GET /products/new
